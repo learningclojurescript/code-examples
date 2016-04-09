22bdd2fec2b0e975cf92e1c16dc8402999ce211d
Allen Rohner <arohner@gmail.com>
2016-04-09 14:03:01 -0500

    Simplify modules, the go chans weren't being used anywhere

--- src/cljs_modules/modules.cljs
+++ src/cljs_modules/modules.cljs
@@ -1,7 +1,5 @@
 (ns cljs-modules.modules
-  (:require [cljs.core.async :as a]
-            [cljs.core.async :as a]
-            [goog.module :as module]
+  (:require [goog.module :as module]
             [goog.module.ModuleManager :as module-manager]
             [goog.module.ModuleLoader]
             [om.core :as om]))
@@ -18,9 +16,6 @@
   #js {"inner" []
        "outer" []})
 
-(def modules-loaded {"inner" (a/chan)
-                     "outer" (a/chan)})
-
 (def manager (module-manager/getInstance))
 (def loader (goog.module.ModuleLoader.))
 
@@ -34,27 +29,17 @@
     false))
 
 (defn require-module
-  "Loads module if necessary. Returns a channel that will be closed once loaded"
   [app id]
-  (let [chan (get modules-loaded id)]
-    (assert chan)
-    (if (not (loaded? id))
-      (do
-        (println "requiring" id)
-        (.execOnLoad manager id (fn []
-                                  (om/update! app [:loaded id] true)
-                                  (-> modules-loaded (get id) (a/close!)))))
-      (a/close! chan))
-    chan))
+  (if (not (loaded? id))
+    (do
+      (println "requiring" id)
+      (om/update! app [:loading id] true)
+      (.execOnLoad manager id (fn []
+                                (om/update! app [:loaded id] true)
+                                (om/update! app [:loading id] false))))))
 
 (defn set-loaded!
   "Mark a module as loaded"
   [id]
   (println "module" id "loaded!" id)
-  (-> goog.module.ModuleManager .getInstance (.setLoaded id))
-  (-> modules-loaded (get id) (a/close!)))
-
-(defn get-load-chan [id]
-  {:pre [(string? id)]
-   :post [%]}
-  (get modules-loaded id))
+  (-> goog.module.ModuleManager .getInstance (.setLoaded id)))
